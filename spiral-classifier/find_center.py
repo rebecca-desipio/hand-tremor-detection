a54c5493e4d10a18d7ec001221d3e4b82402adf2
Rebecca DeSipio <rebeccaelizabeth1721@gmail.com>
2023-03-16 16:35:12 -0400

    3/16/23 updates

--- spiral-classifier/find_center.py
+++ spiral-classifier/find_center.py
@@ -6,7 +6,7 @@ import numpy as np
 
 dir = os.getcwd()
 # imgPath = dir + '/spiral-classifier/skel_1_6_PE0107.jpg'
-imgPath = 'C:/Users/Rebecca/Documents/Virginia_Tech/Research/database-images/database-images/skel/spiral/skel_1_14_PE0184.jpg' #dir + '/datasets/folador_skeletonize/skeletons/waves/skel_V03HO03.png'
+imgPath = 'C:/Users/Rebecca/Documents/Virginia_Tech/Research/database-images/database-images/skel/spiral/skel_1_2_HE097.jpg' #dir + '/datasets/folador_skeletonize/skeletons/waves/skel_V03HO03.png'
 img = cv2.imread(imgPath, 1)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -20,5 +20,5 @@ for x in range(1000):
             img[x][y] = 255
 
 plt.figure(figsize=(15,15))
-plt.matshow(img, fignum=1)
+plt.matshow(img, fignum=1, cmap='gray')
 plt.show()
